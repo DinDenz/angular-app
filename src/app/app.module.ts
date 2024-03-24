0dfd5661b11db759d5d089f91af80992db89ee4c
DinDenz <DinDenz@users.noreply.github.com>
2024-03-24 17:31:12 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { FooterComponent } from './components/UI/footer/footer.component';
 import { HeaderComponent } from './components/UI/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
